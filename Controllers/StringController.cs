82497d5dd07fc1a4c8884ff957894b3a095dfc97
IbrahimMedhat <44685986+IbrahimMedhat@users.noreply.github.com>
2018-11-03 11:46:45 +0200

    Commit 1

---
+++ Controllers/StringController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String Ibrahim()
+        {
+            return "Hello Ibrahim Medhat";
+        }
+
+        public String Welcome()
+        {
+            return "Hello Hi";
+        }
+
+    }
+}
